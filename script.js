4f2d455f291515e722d91d38ff4c30286c8f2dc8
EmersonRabelo <emersonrabelo1998@gmail.com>
2023-06-09 20:15:20 -0300

    Create: Logic of faceRecognition

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-]).then(startWebcam);
+])
+.then(startWebcam)
+.then(faceRecognition);
 
 function startWebcam() {
   navigator.mediaDevices
@@ -21,7 +23,9 @@ function startWebcam() {
 }
 
 function getLabeledFaceDescriptions() {
-  const labels = ["Felipe", "Messi", "Data"];
+
+  const labels = ["Alex", "Emerson"];
+
   return Promise.all(
     labels.map(async (label) => {
       const descriptions = [];
@@ -38,35 +42,39 @@ function getLabeledFaceDescriptions() {
   );
 }
 
-video.addEventListener("play", async () => {
+async function faceRecognition(){
   const labeledFaceDescriptors = await getLabeledFaceDescriptions();
   const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors);
 
-  const canvas = faceapi.createCanvasFromMedia(video);
-  document.body.append(canvas);
-
-  const displaySize = { width: video.width, height: video.height };
-  faceapi.matchDimensions(canvas, displaySize);
-
-  setInterval(async () => {
-    const detections = await faceapi
-      .detectAllFaces(video)
-      .withFaceLandmarks()
-      .withFaceDescriptors();
-
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
-
-    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-
-    const results = resizedDetections.map((d) => {
-      return faceMatcher.findBestMatch(d.descriptor);
-    });
-    results.forEach((result, i) => {
-      const box = resizedDetections[i].detection.box;
-      const drawBox = new faceapi.draw.DrawBox(box, {
-        label: result,
+  video.addEventListener("play", async () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+  
+    const displaySize = { width: video.width, height: video.height };
+    faceapi.matchDimensions(canvas, displaySize);
+  
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(video)
+        .withFaceLandmarks()
+        .withFaceDescriptors();
+  
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+  
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+  
+      const results = resizedDetections.map((d) => {
+        return faceMatcher.findBestMatch(d.descriptor);
       });
-      drawBox.draw(canvas);
-    });
-  }, 100);
-});
+
+      results.forEach((result, i) => {
+        const box = resizedDetections[i].detection.box;
+        const drawBox = new faceapi.draw.DrawBox(box, {
+          label: result,
+        });
+        drawBox.draw(canvas);
+      });
+    }, 100);
+  });
+
+}
